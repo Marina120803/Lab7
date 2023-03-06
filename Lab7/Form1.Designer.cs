244c95e8b1d85fc81e496f29f8a2a2c163adf2ca
Marina120803 <petrenkomarina567@gmail.com>
2023-03-06 15:14:03 +0300

    Добавьте файлы проекта.

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Lab7
+{
+    partial class Lab7
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtPrecision = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(41, 244);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(100, 26);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Вычислить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(41, 80);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.TabIndex = 1;
+            // 
+            // txtPrecision
+            // 
+            this.txtPrecision.Location = new System.Drawing.Point(277, 80);
+            this.txtPrecision.Name = "txtPrecision";
+            this.txtPrecision.Size = new System.Drawing.Size(100, 22);
+            this.txtPrecision.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(50, 167);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(44, 16);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "label1";
+            // 
+            // Lab7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtPrecision);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Lab7";
+            this.Text = "Сумма ряда";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtPrecision;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
